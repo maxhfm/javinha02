bd7731b1fa8567c0fb05dc8e7c6d4ca21a75f4df
Max Henrique <maxhfm@gmail.com>
2017-11-10 05:16:12 -0200

    Biblioteca do seu Ze - alteracao na classe emprestimo

--- src/br/com/bibliotecadoSeuZe/controller/Principal.java
+++ src/br/com/bibliotecadoSeuZe/controller/Principal.java
@@ -10,20 +10,35 @@ import br.com.bibliotecadoSeuZe.model.Pessoa;
 import br.com.bibliotecadoSeuZe.model.SetorBiblioteca;
 
 public class Principal {
-	
+
 	public static void main(String[] args) {
-	
-		Pessoa pessoa = new Pessoa("max", "05788385750");
-		Exemplar[] exemplares = new Exemplar[1];
+
+		Pessoa pessoa = new Pessoa("Max", "05788385750");
 		LocalDate hoje = LocalDate.now();
-		exemplares[0] = new Exemplar("Harry Potter", "novo", hoje, new SetorBiblioteca("1ª Estante", "5ª prateleira"),
-				new Obra("ISBN 978-3-345-78", "Harry Potter", "Rocco", "Fantasia", "1ª Ed", new Autor("J.K.Rowling")));
+
+		Emprestimo emprestimo = new Emprestimo(1, pessoa,
+				new Exemplar("Harry Poter", "livre", hoje, new SetorBiblioteca("1ª estante", "2ª prateleira"),
+						new Obra("BR123-45", "Harry Potter", "Rocco", "Fantasia", "1ªEd", new Autor("J.K.Rowling"))),
+				"emprestado", hoje);
+		emprestimo.emprestimoObra(emprestimo, pessoa.getCpf());
 		
-		Emprestimo emprestimo = new Emprestimo(1, pessoa, exemplares[], "novo", hoje);
+		emprestimo.listaEmprestimo();
+		emprestimo.listaEmprestimoResumida();
+
 		
+		Emprestimo emprestimo2 = new Emprestimo(2, new Pessoa("Lidiane", "11134524714"),
+				new Exemplar("Harry Potter Pedra Filosofal", "livre", hoje, new SetorBiblioteca("1ª estante", "2ª prateleira"),
+						new Obra("BR675-78", "Harry Potter", "Rocco", "Fantasia", "1ªEd", new Autor("J.K.Rowling"))),
+				"emprestado", hoje);
+		emprestimo.emprestimoObra(emprestimo2, pessoa.getCpf());
+		
+		emprestimo.listaEmprestimo();
+		emprestimo.listaEmprestimoResumida();
+		
+		
+		emprestimo.devolucaoObra(emprestimo2, "11134524714", "BR675-78");
+		
+
 	}
-	
-	
-	
 
 }
--- src/br/com/bibliotecadoSeuZe/model/Emprestimo.java
+++ src/br/com/bibliotecadoSeuZe/model/Emprestimo.java
@@ -1,63 +1,96 @@
 package br.com.bibliotecadoSeuZe.model;
 
 import java.time.LocalDate;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class Emprestimo {
 
 	private long numero;
 	private Pessoa pessoa;
 	private Exemplar exemplar;
-	private Exemplar[] exemplares;
 	private String status;
 	LocalDate dataEmprestimo = LocalDate.now();
-	
-	public LocalDate previsaoDevolucao() {
-		return dataEmprestimo.plusDays(7);
+	private List<Emprestimo> emprestimos = new ArrayList<>();
+	private int limiteObraEmprestada = 2;
+
+	public void emprestimoObra(Emprestimo emprestimo, String cpf) {
+		int qtdObraEmprestada = 0;
+
+		for (Emprestimo e : emprestimos) {
+			if (e.getPessoa().getCpf().contains(cpf) && e.getStatus().contains("emprestado"))
+				;
+			qtdObraEmprestada += 1;
+			System.out.println("qtd obra emprestada: " + qtdObraEmprestada + "\n");
+		}
+
+		if (qtdObraEmprestada < limiteObraEmprestada) {
+			emprestimos.add(emprestimo);
+			System.out.println("Livro emprestado");
+		} else {
+			System.out.println("Livro nao emprestado");
+		}
 	}
 
-	public void reciboRegistroEmprestimo(){
-		System.out.println("-------REGISTRO DO EMPRESTIMO-----------");
-		System.out.println("Num do Emprestimo: " + this.numero);
-		System.out.println("Nome: " + this.pessoa.getNome());
-		System.out.println("CPF: " + this.pessoa.getCpf());
-		System.out.println("Qtd de Obras emprestas:" + qtdObrasEmprestada());
-		System.out.println("Obra emprestada: " + exemplares[0]);
-		if(qtdObrasEmprestada() >= 1)
-		System.out.println("Obra emprestada: " + exemplares[1]);
-		System.out.println("Data do Emprestimo: " + dataEmprestimo);
-		System.out.println("----------------------------------------");
-		System.out.println("Data Devolução: " + dataDevolucao());
-		System.out.println("----------------------------------------");
-		System.out.println("Status: " + exemplar.getStatus());
-		
-	}
-	
-	public int qtdObrasEmprestada(){
-		return exemplares.length + 1;
+	public void devolucaoObra(Emprestimo emprestimo, String cpf, String isbn) {
+
+		for (Emprestimo e : emprestimos) {
+			if (e.getExemplar().getObra().getIsbn().contains(isbn) && e.getPessoa().getCpf().contains(cpf)) {
+				System.out.println("indice do cpf consultado: " + emprestimos.indexOf(emprestimo));
+				emprestimos.remove(emprestimo);
+				System.out.println("Emprestimo removido");
+			}else{
+				System.out.println("nao atendeu os parametros");
+			}
+		}
 	}
 
-	public LocalDate dataDevolucao(){
-		return dataEmprestimo.plusDays(7);
+	public void listaEmprestimo() {
+		System.out.println("--------Lista de Emprestimos Completa-----------");
+		for (Emprestimo e : emprestimos) {
+			System.out.println(e.toString());
+		}
 	}
-	
-	@Override
-	public String toString() {
-		return "Emprestimo [numero=" + numero + ", pessoa=" + pessoa + ", exemplares=" + Arrays.toString(exemplares)
-				+ ", status=" + status + ", dataEmprestimo=" + dataEmprestimo + "]";
+
+	public void listaEmprestimoResumida() {
+		System.out.println("--------Lista de Emprestimos-----------");
+		for (Emprestimo e : emprestimos) {
+			System.out.println("Nome: " + e.getPessoa().getNome());
+			System.out.println("CPF: " + e.getPessoa().getCpf());
+			System.out.println("Titulo: " + e.getExemplar().getObra().getTitulo());
+			System.out.println("Editora: " + e.getExemplar().getObra().getEditora());
+			System.out.println("Edição: " + e.getExemplar().getObra().getEdicao());
+			System.out.println("Autor(es): " + e.getExemplar().getObra().getAutores().toString());
+			System.out.println("Status: " + e.getExemplar().getStatus());
+			System.out.println("Data Emprestimo: " + e.getDataEmprestimo() + "\n");
+
+		}
+	}
+
+	public Emprestimo(long numero, Pessoa pessoa, Exemplar exemplar, String status, LocalDate dataEmprestimo) {
+		super();
+		this.numero = numero;
+		this.pessoa = pessoa;
+		this.exemplar = exemplar;
+		this.status = status;
+		this.dataEmprestimo = dataEmprestimo;
+	}
+
+	public Exemplar getExemplar() {
+		return exemplar;
+	}
+
+	public void setExemplar(Exemplar exemplar) {
+		this.exemplar = exemplar;
+	}
+
+	public LocalDate dataDevolucao() {
+		return dataEmprestimo.plusDays(7);
 	}
 
 	public Emprestimo() {
 		super();
-	}
-
-	public Emprestimo(long numero, Pessoa pessoa, Exemplar[] exemplares, String status, LocalDate dataEmprestimo) {
-		super();
-		this.numero = numero;
-		this.pessoa = pessoa;
-		this.exemplares = exemplares;
-		this.status = status;
-		this.dataEmprestimo = dataEmprestimo;
 	}
 
 	public long getNumero() {
@@ -76,14 +109,6 @@ public class Emprestimo {
 		this.pessoa = pessoa;
 	}
 
-	public Exemplar[] getExemplares() {
-		return exemplares;
-	}
-
-	public void setExemplares(Exemplar[] exemplares) {
-		this.exemplares = exemplares;
-	}
-
 	public String getStatus() {
 		return status;
 	}
@@ -100,5 +125,36 @@ public class Emprestimo {
 		this.dataEmprestimo = dataEmprestimo;
 	}
 
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("[numero=");
+		builder.append(numero);
+		builder.append(", pessoa=");
+		builder.append(pessoa);
+		builder.append(", exemplar=");
+		builder.append(exemplar);
+		builder.append(", status=");
+		builder.append(status);
+		builder.append(", dataEmprestimo=");
+		builder.append(dataEmprestimo);
+		builder.append("]");
+		return builder.toString();
+	}
+
+	public void reciboRegistroEmprestimo() {
+		System.out.println("-------REGISTRO DO EMPRESTIMO-----------");
+		System.out.println("Num do Emprestimo: " + this.numero);
+		System.out.println("Nome: " + this.pessoa.getNome());
+		System.out.println("CPF: " + this.pessoa.getCpf());
+		System.out.println("Qtd de Obras emprestas:");
+		System.out.println("Obra emprestada: ");
+		System.out.println("Data do Emprestimo: " + dataEmprestimo);
+		System.out.println("----------------------------------------");
+		System.out.println("Data Devolução: " + dataDevolucao());
+		System.out.println("----------------------------------------");
+		System.out.println("Status: " + exemplar.getStatus());
+
+	}
 
 }
